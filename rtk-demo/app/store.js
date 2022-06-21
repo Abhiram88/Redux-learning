d58d1f701ff5ce6efd2630df07327851f3cce11a
Abhiram88 <76442123+Abhiram88@users.noreply.github.com>
2022-06-21 15:45:26 +0530

    Middleware logger

--- rtk-demo/app/store.js
+++ rtk-demo/app/store.js
@@ -1,12 +1,18 @@
 const configureStore = require('@reduxjs/toolkit').configureStore
+const reduxLogger = require('redux-logger');
 const cakeReducer = require('../features/cake/cakeSlice');
 const iceCreamReducer = require('../features/icecream/iceCreamSlice');
+
+const logger = reduxLogger.createLogger();
 
 const store = configureStore({
     reducer: {
         cake: cakeReducer,
         iceCream : iceCreamReducer
-    }
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger), 
 });
 
 module.exports = store;
+
+// config store function will add some middleware automatically. that is why we use concat
--- rtk-demo/index.js
+++ rtk-demo/index.js
@@ -5,7 +5,7 @@ const iceCreamActions = require('./features/iceCream/iceCreamSlice').IceCreamAct
 console.log('initial state', store.getState());
 
 const unsubscribe = store.subscribe(() => {
-    console.log("updated state", store.getState());
+    //console.log("updated state", store.getState());
 });
 
 store.dispatch(cakeActions.ordered());
