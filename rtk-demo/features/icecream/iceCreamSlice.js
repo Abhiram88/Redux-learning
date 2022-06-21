b68597fd821dccd72df2b7410bee86ebe5008edf
Abhiram88 <76442123+Abhiram88@users.noreply.github.com>
2022-06-21 16:00:15 +0530

    Extra Reducers

--- rtk-demo/features/icecream/iceCreamSlice.js
+++ rtk-demo/features/icecream/iceCreamSlice.js
@@ -1,3 +1,5 @@
+const { cakeActions } = require('../cake/cakeSlice');
+
 const createSlice = require('@reduxjs/toolkit').createSlice
 
 const iceCreamInitialState = {
@@ -14,6 +16,17 @@ const iceCreamSlice = createSlice({
         restocked: (state, action) => {
             state.numOfIceCreams += action.payload
         }
+    },
+    // extraReducers: {
+    //     ['cake/ordered']: (state, action) => {
+    //         state.numOfIceCreams--
+    //     } 
+    // },
+
+    extraReducers: (builder) => {
+        builder: addCase(cakeActions.ordered, state => {
+            state.numOfIceCreams--
+        })
     }
 });
 
